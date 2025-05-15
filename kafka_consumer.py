4de30d121a334911af5b783028b5f1914eb176b6
DeporS <depores1337@gmail.com>
2025-05-15 12:58:19 +0200

    Consumer now saves messages to mongoDB database

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,20 +1,37 @@
 from kafka import KafkaConsumer
 import json
+from pymongo import MongoClient
 
 from config import KAFKA_TOPIC as topic
 
 
-consumer = KafkaConsumer(
-    topic,
-    bootstrap_servers=['localhost:9093'],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-)
+def main():
+    # Connecto to MongoDB
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+    db = mongo_client["events"]
+    collection = db["logins"]
 
-try:
-    for message in consumer:
-        print("Received: ", message.value)
 
-except KeyboardInterrupt:
-        print("Consumer stopped.")
+    # Kafka consumer - receiving messages sent by producer
+    consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=['localhost:9093'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+
+    try:
+        for message in consumer:
+            print(f"Received: {message.value} - saving to MongoDB.")
+            try:
+                collection.insert_one(message.value)
+            except Exception as e:
+                print("Error saving event to db: ", e)
+
+    except KeyboardInterrupt:
+            print("Consumer stopped.")
+
+
+if __name__ == "__main__":
+     main()
